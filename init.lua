11df15a3b9005282ccf17bb0cf925093fd01c447
ezhh <owlecho@live.com>
2017-09-30 19:25:34 +0100

    Enable teleporting

--- init.lua
+++ init.lua
@@ -28,6 +28,11 @@ for i in ipairs(potion_list) do
 		paramtype = "light",
 		groups = {oddly_breakable_by_hand = 3, attached_node = 1},
 		walkable = false,
+		on_rightclick = function(pos, node, player, itemstack, pointed_thing)
+			player:set_attribute("warp_point_"..number, minetest.pos_to_string(pos))
+			minetest.set_node(pos, {name = "air"})
+			return itemstack
+		end
 	})
 
 	minetest.register_craft({
@@ -39,3 +44,27 @@ for i in ipairs(potion_list) do
 		}
 	})
 end
+
+
+-- warp command
+
+minetest.register_chatcommand("warp", {
+	params = "<warp point>",
+	description = "Teleport player to chosen warp point.",
+	func = function(player_name, warp_point)
+		local player = minetest.get_player_by_name(player_name)
+		if minetest.string_to_pos(player:get_attribute("warp_point_"..warp_point)) == nil then
+			minetest.chat_send_player(player_name,"Invalid or un-set warp point.") 
+			return
+		end
+
+		local inv = player:get_inventory()
+		if not inv:contains_item("main", ItemStack("warp_potions:potion_"..warp_point.." 1")) then
+			minetest.chat_send_player(player_name,"You don't have the right potion!")
+			return
+		end
+
+		inv:remove_item("main", ItemStack("warp_potions:potion_"..warp_point.." 1"))
+		player:set_pos(minetest.string_to_pos(player:get_attribute("warp_point_"..warp_point)))
+	end
+})
